ddb57d867cf9cae4e5d6f93d2ed934adb1dcea02
SamuelRufino <samuelrufino0103@gmail.com>
2024-03-14 15:00:03 -0300

    ✨ feat:  redirecionamento

--- listacontato/contatos/views.py
+++ listacontato/contatos/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from .models import Contato
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def addContact(request):
+    if request.method == 'POST':
+        novo_contato = Contato(
+            nome_completo = request.POST['fullname'],
+            relacao = request.POST['relationship'],
+            email = request.POST['email'],
+            numero = request.POST['phone-number'],
+            endereco = request.POST['address'],
+        )
+        novo_contato.save()
+        return redirect('/')
     return render(request, 'new.html')
