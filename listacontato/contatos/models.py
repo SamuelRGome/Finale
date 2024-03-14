db5d73010d656af110a68b3051930d0e509c4467
SamuelRufino <samuelrufino0103@gmail.com>
2024-03-14 12:44:42 -0300

    ✨ feat: adição do models

--- listacontato/contatos/models.py
+++ listacontato/contatos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Contato(models.Model):
+    nome_completo = models.CharField(max_length=500)
+    relacao = models.CharField(max_lenght=50)
+    email = models.EmailField(max_length=254)
+    numero = models.CharField(max_length=20)
+    endereco = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.nome_completo
