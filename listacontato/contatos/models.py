f495bba1f76256f9b3dcdab9acedc864290e90e3
SamuelRufino <samuelrufino0103@gmail.com>
2024-03-14 12:47:26 -0300

    ✨ feat:  migrações

--- listacontato/contatos/models.py
+++ listacontato/contatos/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Contato(models.Model):
     nome_completo = models.CharField(max_length=500)
-    relacao = models.CharField(max_lenght=50)
+    relacao = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     numero = models.CharField(max_length=20)
     endereco = models.CharField(max_length=100)
